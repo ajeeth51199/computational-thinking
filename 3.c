0fcf3e476eee02d4d15ef4cdd0c52b2c5eb040c1
ajeeth51199 <43338045+ajeeth51199@users.noreply.github.com>
2018-09-18 13:40:53 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int i;
+printf("INPUT");
+scanf("%d",&i);
+printf("OUTPUT:");
+if(i=='A'||i=='a'||i=='E'||i=='e'||i=='I'||i=='i'||i=='O'||i=='o'||i=='U'||i=='u'||)
+printf("Vowel");
+else
+printf("constant");
+}
