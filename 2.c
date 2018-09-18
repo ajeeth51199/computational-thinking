5b6ae3c22f94a512ba7e158f59c9e3f19505fa23
ajeeth51199 <43338045+ajeeth51199@users.noreply.github.com>
2018-09-18 12:45:11 +0530

    2.c

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("INPUT");
+scanf("%d",&i);
+if(i%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+}
