4c5236c295d5bbceeed29d761dc571eebab80a72
ajeeth51199 <43338045+ajeeth51199@users.noreply.github.com>
2018-09-18 15:47:55 +0530

    7.c

---
+++ 7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    printf("INPUT:");
+    scanf("%d",&n);
+    printf("OUTPUT:");
+    for(i=1;i<=n;i++)
+    {
+        printf("\nhello");
+    }
+}
